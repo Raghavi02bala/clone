7c910646bc47e7fa688e296c38165914a3b88e5b
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-11-29 10:22:46 +0530

    done

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from './images/xn_logo.jpeg';
 import './Header.css';
-import { AiFillHome } from "react-icons/ai";
+import { AiFillHome, AiFillLike, AiOutlineTags,AiOutlineFieldTime,AiFillFolder } from "react-icons/ai";
 
 function Header() {
     return (
@@ -11,11 +11,26 @@ function Header() {
                     alt="" />
             <div className="header__icons">
             <div className="header__box">
-            <AiFillHome className="header__home"/>
+            <AiFillHome className="header__icon"/>
             <span className="header__text">HOME</span>
             </div>
+            <div className="header__box">
+            <AiFillLike className="header__icon"/>
+            <span className="header__text">HOTTEST VIDEOS</span>
             </div>
-                    
+            <div className="header__box">
+            <AiOutlineTags className="header__icon"/>
+            <span className="header__text">NEWEST VIDEOS</span>
+            </div>
+            <div className="header__box">
+            <AiOutlineFieldTime className="header__icon"/>
+            <span className="header__text">LONGEST VIDEOS</span>
+            </div>
+            <div className="header__box">
+            <AiFillFolder className="header__icon"/>
+            <span className="header__text">CATEGORIES</span>
+            </div>
+            </div>                    
         </nav>
     );
 }
